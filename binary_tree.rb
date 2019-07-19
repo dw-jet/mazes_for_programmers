a354a349c07a1d4e5a545c0512449ca97462447e
James Teitsworth <james.teitsworth@outlook.com>
2019-07-19 08:42:08 -0500

    Change the algo to check to the south and west instead of north and east to reorient the maze

--- binary_tree.rb
+++ binary_tree.rb
@@ -2,8 +2,8 @@ class BinaryTree
     def self.on(grid)
         grid.each_cell do |cell|
             neighbors = []
-            neighbors << cell.north if cell.north
-            neighbors << cell.east if cell.east
+            neighbors << cell.south if cell.south
+            neighbors << cell.west if cell.west
 
             neighbor = neighbors.sample
 
